6be3d91a1b8a4516a3041cecd78891d7be4f65bd
Doug Winsby <dougw@winsbygroup.com>
2020-05-01 22:40:46 -0500

    refactor and work on sync

--- pkg/api/sync/sync.go
+++ pkg/api/sync/sync.go
@@ -23,11 +23,9 @@ package sync
 */
 
 import (
-	"fmt"
 	"net/url"
 
 	"github.com/google/uuid"
-	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 
 	"autocare.org/sandpiper/pkg/api/sync/credentials"
@@ -40,7 +38,7 @@ type subsArray []sandpiper.Subscription
 // Start sends a sync request to a primary sandpiper server from our secondary server
 func (s *Sync) Start(c echo.Context, primaryID uuid.UUID) error {
 	// get current user info from our login token
-	our := s.rbac.CurrentUser(c)
+	// our := s.rbac.CurrentUser(c)
 
 	// must be a secondary server to start the sync
 	if err := s.rbac.EnforceServerRole("secondary"); err != nil {
@@ -55,37 +53,31 @@ func (s *Sync) Start(c echo.Context, primaryID uuid.UUID) error {
 	if err != nil {
 		return err
 	}
-	// get login credentials from company's sync_api_key
-	creds, err := credentials.New(p.SyncAPIKey, s.sec.APIKeySecret())
-	if err != nil {
-		return err
-	}
-	// login to the primary server (saving token)
-	addr, err := url.ParseRequestURI(p.SyncAddr)
-	if err != nil {
-		return err
-	}
-	api, err := client.Login(addr, creds.User, creds.Password, false)
+	// connect to the primary server (saving token)
+	api, err := s.connect(p.SyncAddr, p.SyncAPIKey, s.sec.APIKeySecret())
 	if err != nil {
 		return err
 	}
 
-	// todo: use a websocket connection instead of separate sync http calls to perform the sync process
+	// todo: change sync process to a websocket connection instead of separate sync http calls
 
 	// get our subscriptions from the primary server and add any new ones found
 	primSubs, err := api.AllSubs()
 	if err != nil {
 		return err
 	}
-	localSubs, err := s.sdb.Subscriptions(s.db, our.CompanyID)
+	// get the subscriptions we have for this primary company
+	localSubs, err := s.sdb.Subscriptions(s.db, primaryID)
 	if err != nil {
 		return nil
 	}
+	// add any new subscriptions
 	if err := s.updateOurSubs(localSubs, primSubs, primaryID); err != nil {
 		return err
 	}
 
-	// add any new subscriptions
+	// send a "GET /sync" request to the primary server address
+	// ask for subscriptions (add any not already in our database -- with empty contents)
 
 	/*
 		for _, sub := range subs {
@@ -97,8 +89,6 @@ func (s *Sync) Start(c echo.Context, primaryID uuid.UUID) error {
 		}
 	*/
 
-	// send a "GET /sync" request to the primary server address
-	// ask for subscriptions (add any not already in our database -- with empty contents)
 	/*
 		for each slice in my.slices
 			get slice.hash    // sha1 hash representing slice oids
@@ -115,30 +105,53 @@ func (s *Sync) Start(c echo.Context, primaryID uuid.UUID) error {
 	return nil
 }
 
+func (s *Sync) connect(addr, key, secret string) (*client.Client, error) {
+	// get login credentials from company's sync_api_key
+	creds, err := credentials.New(key, secret)
+	if err != nil {
+		return nil, err
+	}
+	server, err := url.ParseRequestURI(addr)
+	if err != nil {
+		return nil, err
+	}
+	api, err := client.Login(server, creds.User, creds.Password, false)
+	if err != nil {
+		return nil, err
+	}
+	return api, nil
+}
+
+// updateOurSubs makes sure our subscriptions are up-to-date with what the primary has assigned.
+// If a subscription is disabled on the Primary, it will update the Secondary and log the activity.
+// If enabled on the Primary, it will not change the Secondary.
 func (s *Sync) updateOurSubs(ours, prims subsArray, primaryID uuid.UUID) error {
-	// create a map on ours subs for faster lookups [sub_id: sub]
+	// create a map on our subs for fast lookup [sub_id: sub]
 	subs := make(map[uuid.UUID]sandpiper.Subscription)
 	for _, sub := range ours {
 		subs[sub.SubID] = sub
 	}
-	// run through primary subs and add to ours if missing
-	for _, sub := range prims {
-		if _, found := subs[sub.SubID]; !found {
-			sub.CompanyID = primaryID
-			err := s.sdb.AddSubscription(s.db, sub)
+	// run through primary (remote) subs and add to ours (local) if missing
+	for _, remote := range prims {
+		if local, found := subs[remote.SubID]; !found {
+			remote.CompanyID = primaryID // change to our frame of reference for the add
+			err := s.sdb.AddSubscription(s.db, remote)
 			if err != nil {
 				return err
+			}
+		} else {
+			if !remote.Active && local.Active {
+				local.Active = false
+				// todo: update our subscription and log it
+				// err := s.sdb.UpdateSubscription(s.db, local)
 			}
 		}
 	}
 	return nil
 }
 
-// Process (NOT USED) responds to a sync start request and "upgrades" http to a websocket
+// Process (NOT CURRENTLY USED) responds to a sync start request and "upgrades" http to a websocket
 func (s *Sync) Process(c echo.Context) error {
-	var (
-		upgrader = websocket.Upgrader{}
-	)
 
 	if err := s.rbac.EnforceServerRole("primary"); err != nil {
 		return err
@@ -146,29 +159,33 @@ func (s *Sync) Process(c echo.Context) error {
 	if err := s.rbac.EnforceRole(c, sandpiper.CompanyAdminRole); err != nil {
 		return err
 	}
-	// todo: do the actual work here, making calls to s.sdb.xxx as necessary
 
-	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-	if err != nil {
-		return err
-	}
-	defer ws.Close()
+	/*
+		var (
+			upgrader = websocket.Upgrader{}
+		)
 
-	// message loop
-	for {
-		// Write
-		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
+		ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 		if err != nil {
-			c.Logger().Error(err)
+			return err
 		}
+		defer ws.Close()
 
-		// Read
-		_, msg, err := ws.ReadMessage()
-		if err != nil {
-			c.Logger().Error(err)
+		// message loop
+		for {
+			// Write
+			err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
+			if err != nil {
+				c.Logger().Error(err)
+			}
+
+			// Read
+			_, msg, err := ws.ReadMessage()
+			if err != nil {
+				c.Logger().Error(err)
+			}
+			fmt.Printf("%s\n", msg)
 		}
-		fmt.Printf("%s\n", msg)
-	}
-
+	*/
 	return nil
 }
